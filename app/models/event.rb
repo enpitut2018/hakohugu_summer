7351da4e03c905984e0da53f4975351b4a7369dd
Kousuke Uo <s1611427@s.tsukuba.ac.jp>
2018-07-26 14:02:36 +0900

    herokutest9

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :category
   validates :category_id, presence: true
-  validates :title, presence: true, { maximum: 50 }
-  validates :member_n,  { maximum: 20 }
+  validates :title, presence: true, length:{ maximum: 50 }
+  validates :member_n, length:{ maximum: 20 }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   belongs_to :event
-  validates :title, presence: true, { maximum: 50 }
+  validates :title, presence: true, length:{ maximum: 50 }
   validates :deadline, presence: true
   validates :status, presence: true
 end
