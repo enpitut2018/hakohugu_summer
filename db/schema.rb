a9ae455ebc940d9f8764bf01c1e3ca5682feae7f
HinakoIzumi <crossism.libra@gmail.com>
2018-07-26 10:25:38 +0900

    Create sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725050621) do
+ActiveRecord::Schema.define(version: 20180726011631) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20180725050621) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
-    t.float "longtitude"
+    t.float "longitude"
   end
 
 end
